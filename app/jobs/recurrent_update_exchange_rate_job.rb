a52747984aca5da7e17fc600a4c454fea2fb6ec2
Jerry Wisdom <krabique48@gmail.com>
2017-12-15 20:10:43 +0000

    Rubify the recurrent currency update job file

--- app/jobs/recurrent_update_exchange_rate_job.rb
+++ app/jobs/recurrent_update_exchange_rate_job.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class RecurrentUpdateExchangeRateJob < ApplicationJob
   include CommonHelpers
 
   queue_as :default
 
-  after_perform do |job|
+  after_perform do
     # Schedule the update to the next whole hour, i.e. if an initial
     # update was at 13:43:12 today (on the server's start up), the next
     # one will be at 14:00:00 today.
     time = Time.now
     time = time - (time.min * 60) - time.sec + 1.hour
 
-    self.class.set(:wait_until => time).perform_later
+    self.class.set(wait_until: time).perform_later
   end
 
   def perform
