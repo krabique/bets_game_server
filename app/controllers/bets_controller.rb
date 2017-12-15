4691ce67fd7211e03b0cd1285d3dde831f646ff5
Jerry Wisdom <krabique48@gmail.com>
2017-12-15 21:05:51 +0000

    Add a toy check for a banned user

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -35,6 +35,8 @@ class BetsController < ApplicationController
       "Can't bet zero! Show me what you've got, playa'!"
     elsif @account.amount < @bet_amount.to_money(@currency)
       "Insufficient funds. Mah' poor nigga'."
+    elsif random_multiplier(500) == 0
+      "You are banned! You cheater, you!"
     end
   end
 
@@ -60,30 +62,30 @@ class BetsController < ApplicationController
     @bet = Bet.new(complete_params)
   end
 
-  def random_multiplier
+  def random_multiplier(max_value = 2)
     random_org_response = HTTParty.post(
       'https://api.random.org/json-rpc/1/invoke',
-      body: random_org_request_body
+      body: random_org_request_body(max_value)
     )
 
     JSON(random_org_response.body)['result']['random']['data'][0]
   end
 
-  def random_org_request_body
+  def random_org_request_body(max_value)
     {
       'jsonrpc' => '2.0',
       'method' => 'generateIntegers',
-      'params' => random_org_request_body_params,
+      'params' => random_org_request_body_params(max_value),
       'id' => 24_780
     }.to_json
   end
 
-  def random_org_request_body_params
+  def random_org_request_body_params(max_value)
     {
       'apiKey' => ENV['RANDOM_ORG_API_KEY'],
       'n' => 1,
       'min' => 0,
-      'max' => 2,
+      'max' => max_value,
       'replacement' => true,
       'base' => 10
     }
