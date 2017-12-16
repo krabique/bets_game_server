86af48fdf35abbc468f4634d484bc1537a2577bb
Jerry Wisdom <krabique48@gmail.com>
2017-12-16 20:01:50 +0000

    Add routing tests

---
+++ spec/routing/bets_routing_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe BetsController, type: :routing do
+  describe "routing" do
+    it "routes to #create" do
+      expect(:post => "/bets").to route_to("bets#create")
+    end
+  end
+end
