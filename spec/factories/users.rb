4a6fd7272eea64f5df8bf399636f5ed55371752c
Jerry Wisdom <krabique48@gmail.com>
2017-12-16 18:36:54 +0000

    Remove auto-generated tests

--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :user do
-    email             { Faker::Internet.email }
-    password          'abcd1234'
-  end
-end
--- spec/routing/bets_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe BetsController, type: :routing do
-  describe "routing" do
-    it "routes to #create" do
-      expect(:post => "/bets").to route_to("bets#create")
-    end
-  end
-end
