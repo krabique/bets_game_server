5f10389740e5b572faeff656aab230f0fcdd570e
Jerry Wisdom <krabique48@gmail.com>
2017-12-16 22:50:53 +0000

    Add home controller test

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+
+  describe "GET #index" do
+    context 'when user is not logged in' do
+      it "should redirect to the log in page" do
+        get :index
+
+        expect(response.status).to redirect_to :new_user_session
+      end
+    end
+
+    context 'when user is logged in' do
+      let(:user) { create(:user) }
+
+      before(:each) do
+        login_with user
+        get :index
+      end
+
+      it "should set @bet to a new bet" do
+        expect(assigns(:bet).attributes).to eq Bet.new.attributes
+      end
+
+      it "should set @accounts to current user's accounts" do
+        expect(assigns(:accounts)).to eq user.accounts
+      end
+
+      it "should set @bets_log to current user's last 5 bets descending" do
+        5.times { create(:bet) }
+
+        expect(assigns(:bets_log)).to eq user.bets.last(5).reverse
+      end
+
+      it "should set @top_wins to 5 top wins of all time" do
+        5.times { create(:bet) }
+
+        expect(assigns(:top_wins)).to eq(
+          Bet.order('win_amount_eur_cents DESC').limit(5).includes(:user)
+        )
+      end
+
+    end
+  end
+
+end
--- spec/factories/bets.rb
+++ spec/factories/bets.rb
@@ -2,17 +2,25 @@
 
 FactoryBot.define do
   factory :bet do
-    bet_amount          100
-    bet_amount_currency 'EUR'
+    bet_amount              100
+    bet_amount_currency     'EUR'
     user
     account
 
+    multiplier              2
+
+    win_amount              200
+    win_amount_currency     'EUR'
+
+    win_amount_eur          200
+    win_amount_eur_currency 'EUR'
+
     factory :eur_bet do
-      bet_amount_currency 'EUR'
+      bet_amount_currency   'EUR'
     end
 
     factory :usd_bet do
-      bet_amount_currency 'USD'
+      bet_amount_currency   'USD'
     end
   end
 end
