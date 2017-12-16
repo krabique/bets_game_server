fb6999a49bc131d3904bfe1971580fd97e0cc336
Jerry Wisdom <krabique48@gmail.com>
2017-12-16 23:56:34 +0000

    Move random.org api methods to a separate module

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -4,6 +4,7 @@ class BetsController < ApplicationController
   include MoneyHelpers
   include BetParams
   include MaximumBet
+  include RandomHelper
 
   def create
     process_bet
@@ -85,34 +86,5 @@ class BetsController < ApplicationController
   rescue Money::Currency::UnknownCurrency
     @info = ['alert', 'You have to choose a valid currency!']
     false
-  end
-
-  def random_multiplier(max_value = 2)
-    random_org_response = HTTParty.post(
-      'https://api.random.org/json-rpc/1/invoke',
-      body: random_org_request_body(max_value)
-    )
-
-    JSON(random_org_response.body)['result']['random']['data'][0]
-  end
-
-  def random_org_request_body(max_value)
-    {
-      'jsonrpc' => '2.0',
-      'method' => 'generateIntegers',
-      'params' => random_org_request_body_params(max_value),
-      'id' => 24_780
-    }.to_json
-  end
-
-  def random_org_request_body_params(max_value)
-    {
-      'apiKey' => ENV['RANDOM_ORG_API_KEY'],
-      'n' => 1,
-      'min' => 0,
-      'max' => max_value,
-      'replacement' => true,
-      'base' => 10
-    }
   end
 end
