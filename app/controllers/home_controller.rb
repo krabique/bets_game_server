44a20729befc88bcf461a6df4cc3a2d3dc766f51
Jerry Wisdom <krabique48@gmail.com>
2017-12-19 22:01:07 +0000

    Add currency exchange rates info to the top bets chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  include MoneyHelpers
+
   def index
     @bet = Bet.new
     @accounts = current_user.accounts
     @bets_log = current_user.bets.last(5).reverse
     @top_wins = Bet.order('win_amount_eur_cents DESC').limit(5).includes(:user)
+    @exchange_rates = {
+      bank_name: bank.class,
+      updated_at: bank.last_updated,
+      usd: bank.get_rate('EUR', 'USD')
+    }
   end
 end
